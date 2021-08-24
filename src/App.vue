04ca05f179c0d57809ff15080a9437bdb08d74fe
Killian <thevenin.killian@gmail.com>
2021-08-24 19:41:48 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { useHead } from '@vueuse/head'
+useHead({
+  title: 'Vite-key',
+  meta: [
+    { name: 'description', content: 'Opinionated Vite Starter Template' },
+  ],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
