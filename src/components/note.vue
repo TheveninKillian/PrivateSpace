3a89bf72bf94302be6437e3183007df01497782e
Killian <thevenin.killian@gmail.com>
2021-08-26 17:53:17 +0200

    create new component

---
+++ src/components/note.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const props = defineProps({
+  title: String,
+  note: String,
+})
+</script>
+
+<template>
+  <div>
+    <h2>{{ props.title }}</h2>
+
+    <p>{{ props.note }}</p>
+  </div>
+</template>
